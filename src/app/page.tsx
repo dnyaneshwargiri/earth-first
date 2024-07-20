4a9a5550e108dd1ab9b4d056b4c8732e69249cff
Dnyaneshwar Giri <dgiri@foxbase.de>
2024-07-20 15:53:48 +0200

    lib: added libraries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import styles from "./page.module.css";
-
+import { NextUIProvider } from "@nextui-org/react";
+import Avatar from "./avatar";
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <NextUIProvider>
+      {" "}
+      <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Avatar />
+      </main>
+    </NextUIProvider>
+  );
 }
